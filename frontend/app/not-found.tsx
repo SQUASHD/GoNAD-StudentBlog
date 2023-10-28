cfb60f8e26c725a48e674f65d455898aa3a7ff3f
Daniel Hjartland <squashical@gmail.com>
2023-10-28 13:24:12 +0200

    add global not found / 404

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { headers } from "next/headers";
+import { FormattedLink } from "@/components/formatted-primitives";
+
+export default function NotFound() {
+  return (
+    <main className="h-full w-screen flex flex-col items-center justify-center">
+      <h2 className="text-5xl tracking-tighter font-extrabold">404</h2>
+      <p className="text-xl">Could not find requested resource</p>
+      <p>
+        Back to the <FormattedLink href="/">front page</FormattedLink>
+      </p>
+    </main>
+  );
+}
