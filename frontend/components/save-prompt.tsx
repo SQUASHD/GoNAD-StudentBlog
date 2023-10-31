ea8286cae4e8a75f0f900d7612ac54941e41d632
Daniel Hjartland <squashical@gmail.com>
2023-10-31 07:11:08 +0100

    add default onunload warning

---
+++ frontend/components/save-prompt.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function useBeforeUnload() {
+  useEffect(() => {
+    const unloadCallback = (event: BeforeUnloadEvent) => {
+      event.preventDefault();
+      event.returnValue = "";
+      return "";
+    };
+
+    window.addEventListener("beforeunload", unloadCallback);
+    return () => window.removeEventListener("beforeunload", unloadCallback);
+  }, []);
+}
