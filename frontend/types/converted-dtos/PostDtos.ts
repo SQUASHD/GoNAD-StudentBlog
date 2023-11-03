cbc6f18331d2c37d9de0cf4c7201730d07528f9e
Daniel Hjartland <squashical@gmail.com>
2023-11-03 08:11:05 +0100

    update types according to api

--- frontend/types/converted-dtos/PostDtos.ts
+++ frontend/types/converted-dtos/PostDtos.ts
@@ -1,12 +1,20 @@
-export type PostInputReqDto = {
+import { PublicationStatus } from "../internal-enums";
+
+export type CreatePostReqDto = {
   title: string;
   content: string;
+}
+export type UpdatePostReqDto = {
+  title: string;
+  content: string;
+  status: PublicationStatus; // PublicationStatus is an enum, refer to the enum definition for possible values
 }
 export type PostResDto = {
   id: number;
   userId: number;
   title: string;
   content: string;
+  status: PublicationStatus; // PublicationStatus is an enum, refer to the enum definition for possible values
   createdAt: string; // Represents a DateTime type in string format
   updatedAt: string; // Represents a DateTime type in string format
 }
